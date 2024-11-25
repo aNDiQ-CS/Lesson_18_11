710a19b29976861a8e8fa0eb00812efc656ce264
aNDiQ-CS <nikitin.david2013@yandex.ru>
2024-11-25 16:27:52 +0300

    Added second arm
    + Сделали секундную стрелку
    + Сделали плавное движение стрелок

--- Lesson1811/Assets/Clock.cs
+++ Lesson1811/Assets/Clock.cs
@@ -7,6 +7,7 @@ public class Clock : MonoBehaviour
 {
     public Transform HoursArmPivot;
     public Transform MinuteArmPivot;
+    public Transform SecondArmPivot;
 
     private void Start()
     {
@@ -16,7 +17,8 @@ public class Clock : MonoBehaviour
     private void Update()
     {
         DateTime t = DateTime.Now;
-        HoursArmPivot.localRotation = Quaternion.Euler(0, 0, -30 * t.Hour);
-        MinuteArmPivot.localRotation = Quaternion.Euler(0, 0, -6 * t.Minute);
+        HoursArmPivot.localRotation = Quaternion.Euler(0, 0, -30 * t.Hour - (0.5f * t.Minute));
+        MinuteArmPivot.localRotation = Quaternion.Euler(0, 0, -6 * t.Minute - (0.1f * t.Second));
+        SecondArmPivot.localRotation = Quaternion.Euler(0, 0, -6 * t.Second - (0.006f * t.Millisecond));
     }
 }
